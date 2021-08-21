959a578c98f6b821d7e48dc52eaa3e3f92daa95b
Jakub Biesiada <biesiadajakub@icloud.com>
2021-08-21 17:47:31 +0200

    chore: update

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -3,11 +3,18 @@ import { render } from '@testing-library/react-native'
 
 import NativeCircularStatus from '../src'
 
-// TODO
 describe('NativeCircularStatus', () => {
   it('should render component', () => {
     const { toJSON } = render(<NativeCircularStatus progress={0} />)
 
     expect(toJSON()).toMatchSnapshot()
   })
+
+  it.todo('normal thinking')
+  it.todo('normal disabled')
+  it.todo('normal renderContent')
+  it.todo('normal custom iconPause, iconPlay')
+  it.todo('compact')
+  it.todo('compact thinking')
+  it.todo('compact disabled')
 })
